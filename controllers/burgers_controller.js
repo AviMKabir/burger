0920a01d4f8aafde70d65c12b0acc83b363e4c8d
Avi M Kabir <avimishkakabir@gmail.com>
2019-09-20 14:01:23 -0400

    updated controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,7 +24,7 @@ router.post("/api/burgers", function(req, res) {
     req.body.name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+    res.json(result);
   });
 });
 
